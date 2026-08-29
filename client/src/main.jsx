6ec58e44e5d93f363bd64239218ea4caf96df419
Soura Code <roymahapatrauttam@gmail.com>
2026-08-30 01:51:46 +0530

    fix final

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+if ('serviceWorker' in navigator && !(location.hostname === 'localhost' || location.hostname === '127.0.0.1')) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => registration.unregister());
+  }).catch(() => {
+    // Ignore service worker cleanup errors.
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
